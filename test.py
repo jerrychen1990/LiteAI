f00d8f7f7ae33096befa81dd60623fb3487c1b78
jerrychen1990 <jerrychen1990@gmail.com>
2024-08-19 17:11:16 +0800

    添加tool use

--- test.py
+++
@@ -1,5 +0,0 @@
-# messages = [
-#     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-#     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-# ]
-# response = completion(model="gpt-3.5-turbo", messages=messages, stream=True)
